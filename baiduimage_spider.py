4c9cccc8bb6ac1de81e1c016646afce69614d091
Siyuan Li <shulisiyuan@163.com>
2018-11-18 16:48:36 +0800

    百度图片爬虫
    根据关键字爬取百度图片，基于python3
    
    个人学习开发用 提高了爬虫的容错率、稳定性 单线程爬取百度图片结果
    
    安装依赖:pip3 install fake_useragent 图片默认保存在py文件所在路径
    
    运行爬虫：
    
    python baiduimage_spider.py
    输入关键字，用空格分开关键字，同时下载多个关键字的图片
    输入要爬取的页数

---
+++ baiduimage_spider.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+import os
+import re
+import urllib
+import json
+import socket
+import urllib.request
+import urllib.parse
+import urllib.error
+import random
+import linecache
+import time
+
+from fake_useragent import UserAgent
+
+timeout = 5
+ua = UserAgent()
+socket.setdefaulttimeout(timeout)
+
+
+class Crawler:
+    # 睡眠时长
+    __time_sleep = 0.1
+    __amount = 0
+    __start_amount = 0
+    __counter = 0
+
+    headers = {'User-Agent': ua.random}
+
+    # 获取图片url内容等
+    # t表示下载图片时间间隔
+    def __init__(self, t=0.1):
+        self.time_sleep = t
+
+    # 保存图片
+    def __save_image(self, rsp_data, word):
+        if not os.path.exists("./" + word):
+            os.mkdir("./" + word)
+        # 判断名字是否重复，获取图片长度
+        self.__counter = len(os.listdir('./' + word)) + 1
+        for image_info in rsp_data['imgs']:
+            try:
+                time.sleep(self.time_sleep)
+                fix = self.__get_suffix(image_info['objURL'])
+                urllib.request.urlretrieve(image_info['objURL'],
+                                           './' + word + '/' + word + '_' + str(self.__counter) + str(fix))
+            except urllib.error.HTTPError as urllib_err:
+                print(urllib_err)
+                continue
+            except Exception as err:
+                time.sleep(1)
+                print(err)
+                print("产生未知错误，放弃保存")
+                continue
+            else:
+                print('成功下载一张:', word, '图片,已有' + str(self.__counter) + "张图片")
+                self.__counter += 1
+        return
+
+    # 获取后缀名
+    @staticmethod
+    def __get_suffix(name):
+        m = re.search(r'\.[^\.]*$', name)
+        if m.group(0) and len(m.group(0)) <= 5:
+            return m.group(0)
+        else:
+            return '.jpeg'
+
+    # 获取前缀
+    @staticmethod
+    def __get_prefix(name):
+        return name[:name.find('.')]
+
+    # 开始获取
+    def __get_images(self, word='男孩'):
+        search = urllib.parse.quote(word)
+        # pn int 图片数
+        pn = self.__start_amount
+        while pn < self.__amount:
+
+            url = 'http://image.baidu.com/search/avatarjson?tn=resultjsonavatarnew&ie=utf-8&word=' + search + '&cg=girl&pn=' + str(
+                pn) + '&rn=60&itg=0&z=0&fr=&width=&height=&lm=-1&ic=0&s=0&st=-1&gsm=1e0000001e'
+            # 设置header防ban
+            try:
+                time.sleep(self.time_sleep)
+                req = urllib.request.Request(url=url, headers=self.headers)
+                page = urllib.request.urlopen(req)
+                rsp = page.read().decode('unicode_escape')
+            except UnicodeDecodeError as e:
+                print(e)
+                print('-----UnicodeDecodeErrorurl:', url)
+            except urllib.error.URLError as e:
+                print(e)
+                print("-----urlErrorurl:", url)
+            except socket.timeout as e:
+                print(e)
+                print("-----socket timout:", url)
+            else:
+                # 解析json
+                rsp_data = json.loads(rsp)
+                self.__save_image(rsp_data, word)
+                # 读取下一页
+                print("下载下一页")
+                pn += 60
+            finally:
+                page.close()
+        print("下载任务结束")
+        return
+
+    def start(self, word, spider_page_num=10, start_page=1):
+        """
+        爬虫入口
+        :param word: 抓取的关键词
+        :param spider_page_num: 需要抓取数据页数 总抓取图片数量为页数x60
+        :param start_page:起始页数
+        :return:
+        """
+        self.__start_amount = (start_page - 1) * 60
+        self.__amount = spider_page_num * 60 + self.__start_amount
+        self.__get_images(word)
+
+
+if __name__ == '__main__':
+    crawler = Crawler(0)
+    keywords = input('请输入要下载的关键字(使用空格分开关键字): ')
+    keywords = keywords.split(' ')
+
+
+    def input_spider_page_num(keywords):
+        try:
+            spider_page_num = int(input('请输入要爬取主题"%s"的页面数目(每页大约60张图片): ' % keywords))
+        except ValueError:
+            print('输入错误,请输入数字')
+            spider_page_num = input_spider_page_num()
+        return spider_page_num
+
+
+    page_num = input_spider_page_num(keywords=keywords)
+
+    for keyword in keywords:
+        crawler.start(word=keyword, spider_page_num=page_num)
